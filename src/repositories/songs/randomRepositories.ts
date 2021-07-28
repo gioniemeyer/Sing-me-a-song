4f93accf379cc0645e38e55b1d57311116b82b74
leleo <celso@respondeai.com.br>
2021-07-28 17:33:49 -0300

    code review

--- src/repositories/songs/randomRepositories.ts
+++
@@ -1,8 +0,0 @@
-import connection from "../../database";
-
-export async function getSongs() {
-    const response = await connection.query(`
-        SELECT * FROM songs   
-    `)
-    return response.rows
-}
